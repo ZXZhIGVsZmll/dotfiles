e8b3b899d0aee72304650337b2edb66a6c410774
ZXZhIGVsZmll <joshuaapalit@outlook.com>
2023-02-20 22:34:16 +0800

    Update themes.lua

--- nvim/lua/user/themes.lua
+++ nvim/lua/user/themes.lua
@@ -1,6 +1,7 @@
 -- Catppuccin
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 
-vim.cmd [[colorscheme onedark]]
--- vim.cmd [[colorscheme catppuccin]]
+-- vim.cmd [[colorscheme onedark]]
 -- vim.cmd [[colorscheme gruvbox]]
+-- vim.cmd [[colorscheme pink-panic]]
+vim.cmd [[colorscheme catppuccin]]
