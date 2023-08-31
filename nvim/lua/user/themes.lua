f62e299f9f98e4eeaebc274ab0c39222f322d608
ZXZhIGVsZmll <joshuaapalit@outlook.com>
2023-08-31 23:30:12 +0800

    Make background transparent

--- nvim/lua/user/themes.lua
+++ nvim/lua/user/themes.lua
@@ -5,3 +5,4 @@ vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 -- vim.cmd [[colorscheme gruvbox]]
 -- vim.cmd [[colorscheme pink-panic]]
 vim.cmd [[colorscheme catppuccin]]
+vim.cmd [[highlight Normal guibg=none]]
